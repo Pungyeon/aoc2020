9db22e61fe58d36eb6b5abc3510c5f562b2adc33
Pungyeon <lmj@corti.ai>
2020-12-07 19:42:42 +0100

    part two, brute force

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,6 +1,6 @@
 
 use std::fs;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug)]
 enum ValueError {
@@ -27,6 +27,7 @@ fn get_value() -> Result<i64, ValueError> {
         let a = line.parse()?;
         let b = 2020 - a;
         if map.get(&b).is_some() {
+            println!("{} {}", a, b);
             return Ok(a * b);
         }
         map.insert(a, true);
@@ -34,6 +35,29 @@ fn get_value() -> Result<i64, ValueError> {
     Err(ValueError::NoResult)
 }
 
+fn get_values() -> Result<i64, ValueError> {
+    let mut values : Vec<i64> = Vec::new();
+    for line in fs::read_to_string("input.txt")?.lines() {
+        values.push(line.parse()?);
+    }
+
+    for i in 0..values.len() {
+        let a = values.get(i).unwrap();
+        for j in i..values.len() {
+            let b = values.get(j).unwrap();
+            for k in j..values.len() {
+                let c = values.get(k).unwrap();
+                if a + b + c == 2020 {
+                    return Ok(a*b*c);
+                }
+            }
+        }
+    }
+
+    Err(ValueError::NoResult)
+}
+
 fn main() {
-    println!("{:?}", get_value());
+    println!("{:?}", get_value()); // part one
+    println!("{:?}", get_values()); // part two
 }
